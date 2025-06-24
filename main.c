bb4b96bf87fa2f3409d6b9c5026fdc4b8811ce92
Turbobent <kasp728h@edu.mercantec.dk>
2025-06-24 10:14:33 +0200

    check if user input is the text

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <time.h>
 #include <string.h>
 
-int main(){
+//this is a type racer 
+void main(){
+ char typethis[] = "hej med jer\n";
+ char usersInput[100];
 
+ printf("%s", typethis);
+ scanf("%s", usersInput); 
+ if(strcmp(typethis,usersInput) == 0){
+    printf("win");
+ }else{
+  printf("try again");
+ }
 }
